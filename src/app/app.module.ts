7be27e0cc210008e7dcf169bf627dea7a12729d5
Magoratogha <magoratoga@gmail.com>
2023-05-13 18:40:51 -0500

    Added landing page and section 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { Section1Component } from './components/section1/section1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    Section1Component
   ],
   imports: [
     BrowserModule
