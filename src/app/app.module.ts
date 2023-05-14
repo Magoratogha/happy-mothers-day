04376441a8effd4786551068ca97be15d8868fe2
Magoratogha <magoratoga@gmail.com>
2023-05-13 19:29:51 -0500

    Added section 2, 3 and 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { Section1Component } from './components/section1/section1.component';
+import { Section2Component } from './components/section2/section2.component';
+import { Section3Component } from './components/section3/section3.component';
+import { Section4Component } from './components/section4/section4.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    Section1Component
+    Section1Component,
+    Section2Component,
+    Section3Component,
+    Section4Component
   ],
   imports: [
     BrowserModule
