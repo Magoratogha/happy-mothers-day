7dae89ada1eebbb30c2df38596b6ba7e577de9c6
Magoratogha <magoratoga@gmail.com>
2023-05-13 23:00:56 -0500

    Added section 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { Section1Component } from './components/section1/section1.component';
 import { Section2Component } from './components/section2/section2.component';
 import { Section3Component } from './components/section3/section3.component';
 import { Section4Component } from './components/section4/section4.component';
+import { Section5Component } from './components/section5/section5.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { Section4Component } from './components/section4/section4.component';
     Section1Component,
     Section2Component,
     Section3Component,
-    Section4Component
+    Section4Component,
+    Section5Component
   ],
   imports: [
     BrowserModule
